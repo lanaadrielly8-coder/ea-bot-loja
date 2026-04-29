f59524cd92c1140c4858565c5952fc1348303028
lanaadrielly8-coder <lanaadrielly8@gmail.com>
2026-04-29 02:10:25 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const qrcode = require('qrcode-terminal');
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('Bot Online!'));
+app.get('/', (req, res) => res.send('EA Moto Peças Online!'));
 app.listen(process.env.PORT || 3000);
 
 const client = new Client({
     authStrategy: new LocalAuth(),
-  puppeteer: {
-        // ESSA LINHA É A PONTE QUE FALTAVA:
-        executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || null,
+    puppeteer: {
+        // Se houver uma variável de ambiente, ele usa. Se não, tenta o padrão.
+        executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || '/usr/bin/google-chrome-stable',
         args: [
             '--no-sandbox',
             '--disable-setuid-sandbox',
@@ -19,13 +19,15 @@ const client = new Client({
             '--no-zygote'
         ],
     }
+});
+
 client.on('qr', (qr) => {
     qrcode.generate(qr, {small: true});
     console.log('SCANNEIE O QR CODE ABAIXO:');
 });
 
 client.on('ready', () => {
-    console.log('Bot da EA Moto Peças pronto!');
+    console.log('Bot Pronto! A EA Moto Peças está online.');
 });
 
 client.initialize();
