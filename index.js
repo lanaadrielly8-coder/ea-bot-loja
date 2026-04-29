150534a923165abfa7e8605110c0b0be920737c6
lanaadrielly8-coder <lanaadrielly8@gmail.com>
2026-04-29 01:17:27 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,31 +2,19 @@ const { Client, LocalAuth } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('Bot Online!'));
-app.listen(port);
+app.get('/', (req, res) => res.send('Bot Online'));
+app.listen(process.env.PORT || 3000);
 
 const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: {
-        // Isso faz o código procurar o Chrome onde quer que o sistema o instale
-        executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || null, 
+        executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || null,
         handleSIGTERM: false,
-        args: [
-            '--no-sandbox',
-            '--disable-setuid-sandbox',
-            '--disable-dev-shm-usage',
-            '--disable-gpu',
-            '--no-zygote'
-        ],
+        args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage']
     }
 });
-client.on('qr', (qr) => {
-    qrcode.generate(qr, {small: true});
-    console.log('SCANNEIE O QR CODE ABAIXO:');
-});
 
-client.on('ready', () => console.log('Bot da EA Moto Peças pronto!'));
-
+client.on('qr', qr => qrcode.generate(qr, {small: true}));
+client.on('ready', () => console.log('Bot Pronto!'));
 client.initialize();
